a37778f9bf0d554f8666a27b42fd4b6f596f58a2
Kaique da Silva <kaique.developer@gmail.com>
2012-07-17 18:27:08 -0300

    project set up with bootstrap

---
+++ actions/app.py
@@ -0,0 +1,7 @@
+def configuration():
+    return {
+        'title': 'Sinopse Maker',
+        'name': 'Sinopse Maker',
+        'description': 'Create sinopse, and save format json.',
+        'author': 'Kaique da Silva <kaique.developer@gmail.com>'
+    }     
--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template, url_for
+import actions.app
 
 app = Flask( __name__ )
 
 @app.route('/')
 def index():
-    return 'App is running!'
+    conf = actions.app.configuration()
+    return render_template( 'index.html', app=conf )
 
 if __name__ == '__main__':
     app.debug = True
